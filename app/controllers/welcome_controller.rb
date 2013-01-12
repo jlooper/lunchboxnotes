3bd77032bfa39d5de15b18a44d1660823f5aeff2
Jen Looper <jen.looper@gmail.com>
2013-01-12 11:03:13 -0500

    changing image rotation strategy from js to ruby

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,16 @@
 class WelcomeController < ApplicationController
+
+	
 	def index
     @notes = Note.all.shuffle
 	
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @notes }
+    format.html # index.html.erb
+    format.json { render json: @notes }
+
+    @images  = ["bgNotes1.png", "bgNotes2.png", "bgNotes3.png", "bgNotes4.png", "bgNotes5.png","bgNotes6.png","bgNotes7.png","bgNotes8.png","bgNotes9.png","bgNotes10.png"]
+    @random_no = rand(10)
+    @random_image = @images[@random_no]
     end
   end
 end
